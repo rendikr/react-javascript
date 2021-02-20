5054cddecb4a66f2848be59b075518edafde6c83
Rendi K <rendi.krinaldi@gmail.com>
2021-02-20 13:12:27 +0700

    basic app layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  const userLogged = "Rendi K.";
-  const userLoggedJSX = <strong>Rendi K.</strong>;
+  const category = "Canvas Shoes";
+  const title = "Chuck"
+  const price = 1300000;
+  const info = "One of the most recognizable shoes in the world."
 
   return (
-    <div className="App">
-      {userLogged}
-      {userLoggedJSX}
+    <div className="Parent-box">
+      <div className="Photo">
+        <img src="logo192.png"></img>
+      </div>
+      <div className="Description">
+        <p className="Category">{category}</p>
+        <h1 className="Title">{title}</h1>
+        <p className="Price">IDR {price}</p>
+        <p className="Info">
+          {info}
+        </p>
+      </div>
     </div>
   );
 }
